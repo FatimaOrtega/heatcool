bf6c58d71c1c0b847a478a293f8a56d2a34f9246
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-03-17 17:22:57 -0500

    Finished first draft of the website

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Navbar from './../Navbar/Navbar';
-
+import papa from "../../assets/papa.jpeg";
+import "./About.css";
 
 const About = () => {
 return (
 	<>
 <Navbar/>
+<div className='about'> 
+	
+	   
+    <div className='about-container'>
+    <div className='about-title'> 
 	<h1>About Us</h1>
+    </div>
+    <div className='about-info'>
+    <h2>A-Ortega Heating & Cooling Inc. once the founder of the company was tired of working the same life as a chef. Ortega felt like he had a passion for something else but yet was not aware of what it was yet. It was until he began to help others with installation, he understood his passion of wanting to become an entrepreneur. The company continues to grow and continue giving the best customer service!</h2>
+   
+    </div>
+	
+	<img src={papa} />
+    </div> 
+    </div>
 	</>
 )
 };
--- src/components/feature/features.js
+++ src/components/feature/features.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import Navbar from './../Navbar/Navbar';
+import "./features.css";
 
 const features = () => {
 return (
 	<>
 <Navbar/>
+<div className='service'>
+	<div className='service-container'>
+	<div className='service-title'>
 	<h1>Services</h1>
+	</div>
+	<ul>
+	<h3>
+		<div className='service-left-side'>
+		<li>Installation:
+			<h2>
+	<ul>Furnace</ul>
+	<ul>Air Conditioner</ul>
+	<ul>Water Heating</ul>
+	<ul>Ducts</ul>
+	<ul>Thermostat</ul>
+	</h2>
+</li>
+		<li>Cleaning:
+			<h2>
+			<ul>Dryer</ul>
+			<ul>Duct</ul>
+			<ul>Unit Maintenance</ul>
+			</h2>
+		</li>
+
+		<li>Humidifier</li>
+		
+
+</div>
+</h3>
+
+</ul>
+	</div>
+	</div>
 	</>
 )
 };
