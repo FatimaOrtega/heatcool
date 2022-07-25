86770b5a608a0591e9c96498c5e16ab5d4426fc7
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-07-25 13:30:44 -0700

    Changed the logos and tweaked the About page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import Navbar from './../Navbar/Navbar';
 import papa from "../../assets/papa.jpeg";
+import blue from "../../assets/blue.svg";
+import red from "../../assets/red.svg";
+import '../../containers/header/header.css';
+import '../feature/features.css';
 import "./About.css";
 
 const About = () => {
 return (
 	<>
 <Navbar/>
-<div className='about'> 
-	
-	   
-    <div className='about-container'>
-    <div className='about-title'> 
-	<h1>About Us</h1>
+<div className='Header'>
+  <div className='Header-BodyContainer'>
+   <div className='Header-BodyContainer-Left'>
+     <h1> 
+      Chicago Land's Family Company
+     </h1>
+
+     <p>Provides the best experience for Heating & Cooling services. Our team is committed to give the best customer satisfaction, we guarantee to give the best service!</p>
+     
+     <div className='Header-BodyContainer-Left-ButtonsContainer'>
+      
+       </div>
+       <div className="Header-van">
+       <img src={papa} />
+         </div>
+     </div>
+
+     <div className='Header-BodyContainer-Right'>  
+      <div className='Header-red'>
+     <img src={red}/>
+     </div>
+     <div className='App-bluePlacement '>
+     <div className='Header-blue'>
+<img src={blue}/>
+</div>
+</div>
+     </div>
+
     </div>
-    <div className='about-info'>
-    <h2>A-Ortega Heating & Cooling Inc. once the founder of the company was tired of working the same life as a chef. Ortega felt like he had a passion for something else but yet was not aware of what it was yet. It was until he began to help others with installation, he understood his passion of wanting to become an entrepreneur. The company continues to grow and continue giving the best customer service!</h2>
-   
-    </div>
-	
-	<img src={papa} />
-    </div> 
-    </div>
+ 
+
+
+ </div>
+
+    
 	</>
 )
 };
